7f6078ea6aa95791e1e16aac6b27d162dbcfce14
Emiliano Cararo <emicararo92@gmail.com>
2023-07-10 14:10:24 -0300

    carga - js

--- Simulador Interactivo/simulador.js
+++ Simulador Interactivo/simulador.js
@@ -1,41 +1,76 @@
-let socio = Number(prompt("Bienvenido al centro de referidos del Club Atletico Belgrano. Ingresa la cantidad de socios referidos (Maximo 4 referidos por socio), y calcula tu descuento en el abono semestral. \nCuantos socios referiste?"))
-
-const cuota = 40000
-
+//declaracion de variables para el socio que refiere y las cuotas.
 
 function club() {
-    while (socio > 0 && socio < 5) {
+/* 
+    let socio;
+    const cuota = 40000
+
+    while (true) {
+        socio = prompt(`Bienvenido a la pagina del club atletico Belgrano.
+        \nIngresa la cantidad de socios que referiste (Hasta 4 referidos por socio) por cada socio referido tenes un 5% de descuento en tu abono semestral
+        \n¿Cuantos socios referiste?
+        \n Ingresa el numero de socios referidos, y conoce tu descuento!`)
+
+        if (socio === "" || socio === " ") {
+            alert("No ingresaste ningun numero")
+        } else if (socio === null) {
+            alert("Gracias por vistar la pagina del club atletico Belgrano")
+            break
+        }
+
+        socio = Number(socio)
+
         if (socio === 1) {
-            let des1 = cuota - (cuota * 0.05)
-            alert("Tenes el 5% de descuento en tu abono! El total a abonar de este semestre es de " + des1)
-            break
-            
-
-
-        }
-        else if (socio === 2) {
-            let des2 = cuota - (cuota * 0.10)
-            alert("Tenes el 10% de descuento en tu abono! El total a abonar de este semestre es de " + des2)
-            break
-
-
-
+            let des1 = cuota - cuota * 0.05;
+            alert(
+                "Tenes el 5% de descuento en tu abono! El total a abonar de este semestre es de " +
+                des1
+            );
+        } else if (socio === 2) {
+            let des2 = cuota - cuota * 0.1;
+            alert(
+                "Tenes el 10% de descuento en tu abono! El total a abonar de este semestre es de " +
+                des2
+            );
         } else if (socio === 3) {
-            let des3 = cuota - (cuota * 0.15)
-            alert("Tenes el 15% de descuento en tu abono! El total a abonar de este semestre es de " + des3)
-            break
-
+            let des3 = cuota - cuota * 0.15;
+            alert(
+                "Tenes el 15% de descuento en tu abono! El total a abonar de este semestre es de " +
+                des3
+            );
         } else if (socio === 4) {
-            let des4 = cuota - (cuota * 0.20)
-            alert("Tenes el 20% de descuento en tu abono! El total a abonar de este semestre es de " + des4)
-           break
+            let des4 = cuota - cuota * 0.2;
+            alert(
+                "Tenes el 20% de descuento en tu abono! El total a abonar de este semestre es de " +
+                des4
+            );
+        } else if (socio > 4) {
+            alert("no se puede referir mas de 4 personas");
+        } else {
+            break;
+        }
+    } */
 
+    let amorchis;
+
+    while(true){
+        amorchis = prompt("Es usted un amorchis? SI/NO")
+
+        if (amorchis == "" || amorchis === " "){
+            alert("No ingresaste nada... extraño")
+        }else if(amorchis === null) {
+            alert("naaa no seas mala, pone si")
         }
 
-        
+        if(amorchis === "si"){
+            alert("Eres muy preciosa")
+            break
+        }else if(amorchis === "no"){
+            alert(" Si, si si lo sos, y solo podras irte de aca si dices que Si jaajajaj.")
+        }
+    }   
 
 
-    }
 }
 
 club()
